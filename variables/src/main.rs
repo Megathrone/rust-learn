ea5ca43d489f33488d6b636312efc655e9109815
Megathrone <yizhouwang418@gmail.com>
2019-08-03 23:02:40 +0800

    data type

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,4 @@
 fn main() {
-    const MAX_VALUE: u32 = 500;
-    let x = 5;
-    println!("The value of x is: {}", x);
-
-    println!("{}", MAX_VALUE);
+    let strarr = ["nmsl";5];
+    println!("{:?}",strarr);
 }
